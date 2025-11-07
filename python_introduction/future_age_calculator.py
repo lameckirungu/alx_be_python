ef8aa833a06a92a39d93b275800447d0aa3b64f2
Lameck Irungu <mugolameck@gmail.com>
2025-11-07 07:02:13 +0300

    calculates users age from input

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+"""
+Task: Create a Python script that asks the user for their current age and then calculates how old they will be in a specific future year.
+"""
+
+current_user_age = int(input("How old are you? "))
+age_in_2050 = current_user_age + 27 # Assumption: current_year = 2023
+
+print("In 2050, you will be", age_in_2050, "years old.")
