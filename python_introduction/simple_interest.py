5935b9747eb6cb24eb32bed2c50d5d0aee93e2ba
Lameck Irungu <mugolameck@gmail.com>
2025-11-07 06:44:28 +0300

    feat: calculates simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+"""
+Task: Write a python script that calculates the simple interest 
+earned on an investment over a period of time.
+
+Formula: Simple Interest = (Principal * Rate * Time) / 100
+"""
+
+principal = 1000 # in $
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
