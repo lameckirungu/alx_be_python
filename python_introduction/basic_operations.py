1716d81a1071b671550bf46da0b82121f70303dd
Lameck Irungu <mugolameck@gmail.com>
2025-11-07 06:34:18 +0300

    adds basic arithmetic ops

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# Basic Arithmetic Operations
+
+number1 = 12
+number2 = 30
+
+addition  = number1 + number2
+multiplication = number1 * number2
+difference = number1 - number2
+
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
