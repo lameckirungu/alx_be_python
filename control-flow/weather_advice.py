7e7b809f1c1e206c6df11821c84f77ab2bb522b2
Lameck Irungu <mugolameck@gmail.com>
2025-11-11 19:34:28 +0300

    Update weather program to use str directly

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,10 @@
-weather_conditions = ["sunny", "rainy", "cold"]
-
 user_weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if user_weather == weather_conditions[0]:
+if user_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif user_weather == weather_conditions[1]:
+elif user_weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif user_weather == weather_conditions[2]:
+elif user_weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
