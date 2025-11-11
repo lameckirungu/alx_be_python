f09f5c27cced376c4301eafc04a659bfb37b35f3
Lameck Irungu <mugolameck@gmail.com>
2025-11-11 22:21:39 +0300

    Fix: modified variable names to match test cases

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,12 +1,12 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-user_operator = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-if user_operator == '/' and num2 == 0:
+if operation == '/' and num2 == 0:
     print("Cannot divide by zero.")
 else:
-    match user_operator:
+    match operation:
         case '+':
             result = num1 + num2
             print(f"The result is {result}.")
