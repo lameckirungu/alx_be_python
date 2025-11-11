d3e999163a282dfebf583d42032bf51191724dbe
Lameck Irungu <mugolameck@gmail.com>
2025-11-11 20:06:50 +0300

    fix: match required program string descriptions

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,24 +1,23 @@
-import sys
-
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-       
-user_operator = input("Choose the operation (+, -, *, /): ")
-if num2 == 0 and user_operator == '/':
-    print("Cannot divide by zero.")
-    sys.exit(1)
 
-match user_operator:
-    case '+':
-       result = num1 + num2
-    case '-':
-       result = num1 - num2
-    case '*':
-        result = num1 * num2
-    case '/':
-        result = num1 / num2
-    case _:
-        print("Invalid operator.")
-        sys.exit(1)
-    
-print(f"The result is {result}.")
+user_operator = input("Choose the operation (+, -, *, /): ")
+
+if user_operator == '/' and num2 == 0:
+    print("Cannot divide by zero.")
+else:
+    match user_operator:
+        case '+':
+            result = num1 + num2
+            print(f"The result is {result}.")
+        case '-':
+            result = num1 - num2
+            print(f"The result is {result}.")
+        case '*':
+            result = num1 * num2
+            print(f"The result is {result}.")
+        case '/':
+            result = num1 / num2
+            print(f"The result is {result}.")
+        case _:
+            print("Invalid operator")
