b4237d5399accd77904e5965dfd2d4704b0a91c3
Lameck Irungu <mugolameck@gmail.com>
2025-11-23 12:35:59 +0300

    fix: remove trailing characters to keep exact text string required to pass checker tests

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,14 +1,10 @@
-"""
-"""
-
 def display_menu():
     """Prints the main menu options to the console."""
-    print("\n--- Shopping List Manager ---")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-    print("---------------")
 
 def main():
     shopping_list = []
