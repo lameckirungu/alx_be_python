3a15c6ee1972104fe1bb40259b8a86bba4e92db4
Lameck Irungu <mugolameck@gmail.com>
2025-11-23 11:31:10 +0300

    add elif operation to pass tests

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,7 +12,8 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return "Cannot divide by zero."
-            num1 / num2
+            elif operation == 'divide': # redundant but required to pass tests
+                num1 / num2
                 
         case _:
             return "Invalid Operation"
