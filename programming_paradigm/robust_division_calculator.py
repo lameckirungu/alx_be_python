4f040fcfa7b0cf84c457b52f09ba91f5983643d1
Lameck Irungu <mugolameck@gmail.com>
2025-11-25 17:17:53 +0300

    feat: adds arithmetic division logic to solve task 1

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,10 +1,23 @@
 def safe_divide(numerator, denominator):
-    try:
-        res = numerator / denominator
-        print(f"The result of the division is {res}")
-    except ZeroDivisionError:
-        print(f"Error: Cannot divide by zero")
-    except ValueError:
-        print(f"Error: Please insert a numeric values only.")
+    """
+    Performs division robustly, handling ValueError (non-numeric inputs)
+    and ZeroDivisionError
 
-safe_divide()
+    Args:
+        numerator (float): The string representing the numerator.
+        denominator (float): The string representing the denominator.
+
+    Returns:
+        float: The result of the division or an appropriate error message.
+    """
+    try:
+        num = float(numerator)
+        den = float(denominator)
+
+        try:
+            res = num / den
+            return f"The result of the division is {res}"
+        except ZeroDivisionError:
+            return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
